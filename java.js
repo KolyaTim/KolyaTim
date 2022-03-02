7561db5a38f0e846f7a81f9e4dce5ea10caaeaee
Nickolay Kharlov <novoselov_t@list.ru>
2022-03-02 12:50:50 +0700

    java smeshariki

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 let students = []
-let current = 0
+let max_id = -1 
 
 //загрузить данные студент по его id
 function loadFromSite() {
@@ -8,17 +8,37 @@ function loadFromSite() {
 		show_table()
 	});
 }
-function show_table(){
+function show_table() {
 	let table = $('#tbl_all')
 	for (let i=0; i < students.length; i++){
+		if (i > max_id) {
+			max_id = i
 		//
 		let tr = $('<tr></tr>')
 		let td1 = $('<td>' + students[i].id + '</td>')
 		let td2 = $('<td>' + students[i].surname + '</td>')
 		let td3 = $('<td>' + students[i].name + '</td>')
+		let td4 = $('<td><button onclick="show_info('+i+')">Подробно</button></td>')
 		//
-		tr.append(td1).append(td2).append(td3)
+		tr.append(td1).append(td2).append(td3).append(td4)
 		//
 		table.append(tr)
 	}
+	}
+}
+
+function show_info(id) {
+	//
+	let div = $('#info')
+
+	//
+	let header = $('<h1>Информация о студентем № '+id+'</h1>')
+	//
+	let name = $('<h3>'+students[id].name+' '+students[id].surname+'</h3>')
+	//
+	let img = $('<img src="http://217.71.129.139:4035/'+students[id].logo+'">')
+	//
+	div.html('')
+	//
+	div.append(header).append(name).append(img)
 }
