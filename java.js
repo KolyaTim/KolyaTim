84f338281226137f7d68767d7b5d3a233238ef61
Nickolay Kharlov <novoselov_t@list.ru>
2022-03-02 12:06:43 +0700

    java 3

--- java.js
+++ java.js
@@ -5,18 +5,20 @@ let current = 0
 function loadFromSite() {
 	$.get('http://217.71.129.139:4035/students.php', function(data){
 		students = JSON.parse(data)['response'];
+		show_table()
 	});
 }
-function load_students(id) {
-	$('#name').val(students[id].name)
-	$('#surname').val(students[id].surname)
-}
-	
-//следующий студент
-function next() {
-	load_students(current)
-	current++
-
-if (current === students.length){
+function show_table(){
+	let table = $('#tbl_all')
+	for (let i=0; i < students.length; i++){
+		//
+		let tr = $('<tr></tr>')
+		let td1 = $('<td>' + students[i].id + '</td>')
+		let td2 = $('<td>' + students[i].surname + '</td>')
+		let td3 = $('<td>' + students[i].name + '</td>')
+		//
+		tr.append(td1).append(td2).append(td3)
+		//
+		table.append(tr)
 	}
 }
